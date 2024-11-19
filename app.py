86658377eb267ff03aa96710cb30670272037b14
mr-shrayan <sbanerjee22@outlook.com>
2024-11-19 15:42:42 +0530

    Register and login working fine

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def register():
 
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         val = (name, email, phone, hashed_password)
-
+        print(name, email, phone, hashed_password)
         try:
             mydb = mysql.connector.connect(
                 host="localhost",
@@ -32,14 +32,51 @@ def register():
             mydb.commit()
             mycursor.close()
             mydb.close()
-
+            print(name, email, phone, hashed_password)
             return redirect(url_for('login'))  # Redirect to login page or other success page
         
         except mysql.connector.Error as err:
             print(f"Error: {err}")
-            return render_template('error.html', error_message=str(err))
+            # return render_template('error.html', error_message=str(err))
 
     return render_template('register.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password'] 
 
+
+        # Connect to the database
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="admin",
+            database="projectsmile"
+        )
+
+        mycursor = mydb.cursor()
+
+
+        # Check if user exists
+        sql = "SELECT * FROM users WHERE email = %s"
+        val = (email,)
+        mycursor.execute(sql, val)
+
+        user = mycursor.fetchone()
+
+        if user:
+            hashed_password = user[4]
+            # print(hashed_password)
+            # print(bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()))  # Assuming password is at index 4 (id,name, email, phone, password)
+            if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+                # Successful login, redirect to dashboard or other page
+                return render_template('home.html')
+            else:
+                return render_template('error.html')
+        else:
+            return redirect(url_for('register'))  # Redirect to registration page
+
+    return render_template('login.html')
 if __name__== '__main__':
     app.run(debug=True)
